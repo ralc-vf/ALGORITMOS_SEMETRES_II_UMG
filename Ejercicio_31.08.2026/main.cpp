93fae778a887ec7f2a6f698cde0f296d2e2db949
ralc-vf <rlopezc52@miumg.edu.gt>
2026-09-04 08:52:11 -0600

    Se adjunta el Ejercicio

---
+++ Ejercicio_31.08.2026/main.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+
+//Prototipos
+void ingrasarNotas (int notas[], int TAM);
+void mostrarNotas(int notas[], int TAM);
+
+int main(){ 
+    const int TAM = 5;
+
+
+
+    return 0;
+}
+
+
+void mostrarNotas(int notasMotrar[], int TAM){
+    //Mostrar las Notas
+    cout << "\n==============================" << endl;
+    cout << "Notas Registradas" << endl;
+    cout << "\n==============================" << endl;
+}
